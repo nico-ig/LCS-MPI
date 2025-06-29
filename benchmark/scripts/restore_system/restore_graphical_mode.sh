b85279353ff83589d8634d4ad4d441f03cf9aa8f
Nico Ramos <nigr21@inf.ufpr.br>
2025-06-28 23:08:00 -0300

    [REMOVE] Old files

--- benchmark/scripts/restore_system/restore_graphical_mode.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Restoring default system mode to graphical.target..."
-sudo systemctl set-default graphical.target
-echo "You can reboot your system with:"
-echo "    sudo reboot"
--- benchmark/scripts/setup_system/deactivate_hyper_threading.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Deactivating Hyper-Threading..."
-echo off | sudo tee /sys/devices/system/cpu/smt/control > /dev/null
-echo "Hyper-Threading deactivated."
-./scripts/utils/check_hyper_threading.sh
